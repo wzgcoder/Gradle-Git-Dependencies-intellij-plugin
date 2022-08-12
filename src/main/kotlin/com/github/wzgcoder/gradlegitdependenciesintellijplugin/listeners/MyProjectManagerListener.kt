ef6fb40bd8d2e2368fac6916ae69ea73f6b429e0
GitHub Action <action@github.com>
2022-08-12 06:58:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/wzgcoder/gradlegitdependenciesintellijplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/wzgcoder/gradlegitdependenciesintellijplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.wzgcoder.gradlegitdependenciesintellijplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.wzgcoder.gradlegitdependenciesintellijplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/wzgcoder/gradlegitdependenciesintellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wzgcoder/gradlegitdependenciesintellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wzgcoder.gradlegitdependenciesintellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wzgcoder.gradlegitdependenciesintellijplugin.MyBundle
 
 class MyApplicationService {
 
